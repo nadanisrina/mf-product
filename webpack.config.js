9b8d4c30c9b5135872a754eec88def5a09db1fc3
nadans <nisrinaseptiana1@gmail.com>
2021-11-01 17:23:57 +0700

    set module federation plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
 let HTMLWebpackPlugin = require("html-webpack-plugin");
+let ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 module.exports = {
   mode: "development",
   devServer: {
-    port: 8081,
+    port: "8081",
   },
   plugins: [
+    new ModuleFederationPlugin({
+      name: "products",
+      filename: "remoteEntry.js",
+      //will get expose to remote app
+      exposes: {
+        "./ProductsIndex": "./src/index",
+      },
+    }),
     new HTMLWebpackPlugin({
       template: "./public/index.html",
     }),
